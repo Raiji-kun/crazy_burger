2295bd505d22ac24acc5f020d5bd38a6fb9441db
Raiji-kun <huot.edouard@gmail.com>
2023-07-31 12:47:00 +0900

    ✨ F01 : create button and form

--- src/Components/Pages/LoginPage.jsx
+++ src/Components/Pages/LoginPage.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react"
+
 export default function LoginPage() {
+
+    const [text, setText] = useState("")
+  
+    const handleButton = (event) => {
+      event.preventDefault()
+      alert("Bonjour " + text   )
+      setText("")
+    }
+  
+    const handleText = (event) => {
+      setText(event.currentTarget.value)
+    }
   return (
-    <div>LoginPages</div>
+    <div>
+
+    <h1>Bienvenue chez nous</h1>
+    <h2>Connectez vous</h2>
+    <form onSubmit={handleButton}>
+        <input type='text' value={text} onChange={handleText} placeholder='entrez votre nom'  required />
+        <button  >Accedez a votre espace</button>
+    </form>
+    </div>
   )
 }
