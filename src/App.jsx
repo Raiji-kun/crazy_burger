b58b124cef73be95363c4fa740e84c3bc16dca49
Raiji-kun <huot.edouard@gmail.com>
2023-07-31 19:05:09 +0900

    test

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  const [text, setText] = useState("")
+  
+  const handleButton = (event) => {
+    event.preventDefault()
+    alert("Bonjour " + text   )
+    setText("")
+  }
+
+  const handleText = (event) => {
+    setText(event.currentTarget.value)
+  }
+
+  return (
+    <div>
+      <h1>Bienvenue chez nous</h1>
+      <h2>Connectez vous</h2>
+      <form onSubmit={handleButton}>
+        <input type='text' value={text} onChange={handleText} placeholder='entrez votre nom'  required />
+        <button  >Accedez a votre espace</button>
+        </form>
+    </div>
+    
+  )
+}
+
+export default App
